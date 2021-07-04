322d2bfc3bd3b9338a05cc341ce9b02d33e04f42
minki-kim-git <rlaalsrl1227@gmail.com>
2021-07-05 00:49:00 +0900

    스위프트 문법연습

---
+++ ex_01_grammar.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+//함수
+func hazard() -> Int {
+    return 3
+}
+func name(front:String,back:String) -> String{
+    return "my name is \(front) \(back) ."
+    
+}
+print(hazard())
+print(name(front: "KIM", back: "Mingi"))
+
+func output() {
+print("잘 출력되나요?")
+}
+output()
+
+func sum(num1:Int,num2:Int) ->Int{
+
+ return num1+num2
+}
+print(sum(num1: 312312, num2: 312312312))
+
